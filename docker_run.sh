6328e17d97a203106f9fa47b96a5b7cb78027a56
mohneesh-9797 <62938477+mohneesh-9797@users.noreply.github.com>
2020-05-13 17:01:10 +0530

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 srcdir=$( cd "$(dirname $0)"  && pwd )
 echo "srcdir=$srcdir"
-docker run --rm -v $srcdir:/root/jobber alpine:latest /root/jobber/build_alpine.sh
+sudo docker run --rm -v $srcdir:/root/jobber alpine:latest /root/jobber/build_alpine.sh
