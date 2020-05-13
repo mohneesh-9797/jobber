d248f31d1dbe7b19f446d95f31f71356b3c3620b
mohneesh-9797 <62938477+mohneesh-9797@users.noreply.github.com>
2020-05-13 20:01:26 +0530

    Update build_alpine.sh

--- build_alpine.sh
+++ build_alpine.sh
@@ -12,7 +12,7 @@ apk add git
 sudo --version 
 adduser --disabled-password  mohneesh
 sh -c "echo 'sudo	ALL=(ALL) ALL'" >> /etc/sudoers
-sudo sh -c "echo 'mohneesh ALL=NOPASSWD: ALL' >> /etc/sudoers"
+sudo sh -c "echo 'mohneesh ALL=(ALL)NOPASSWD: ALL' >> /etc/sudoers"
 cat /etc/sudoers
 su mohneesh
 sudo git clone https://github.com/mohneesh9797-puresoftware/jobber/
