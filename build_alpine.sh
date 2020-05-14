fee86bb8e0d41c520d8fdbf76a3f0cbbba111995
mohneesh-9797 <62938477+mohneesh-9797@users.noreply.github.com>
2020-05-14 12:40:09 +0530

    Update build_alpine.sh

--- build_alpine.sh
+++ build_alpine.sh
@@ -22,7 +22,7 @@ mkdir –p /var/cache/distfiles
 chmod a+w /var/cache/distfiles
 chgrp abuild /var/cache/distfiles
 chmod g+w /var/cache/distfiles
-su mohneesh
+su - mohneesh
 echo " su mohneesh "
 whoami
 cd /tmp/jobber/packaging/alpine 
