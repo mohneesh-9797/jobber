5adb308b4b79bdc5546d655ae72089ba0f4969b6
mohneesh-9797 <62938477+mohneesh-9797@users.noreply.github.com>
2020-05-19 13:59:04 +0530

    Update build_alpine.sh

--- build_alpine.sh
+++ build_alpine.sh
@@ -1,4 +1,3 @@
-echo "hello";
 apk update
 apk add go rsync make git gcc musl-dev alpine-sdk
 adduser --disabled-password  mohneesh
@@ -11,13 +10,8 @@ mkdir –p /var/cache/distfiles
 chmod a+w /var/cache/distfiles
 chgrp abuild /var/cache/distfiles
 chmod g+w /var/cache/distfiles
-cd /tmp/jobber/packaging/
-chmod -R a+w alpine/
-chmod a+w alpine/APKBUILD
 addgroup  mohneesh abuild
-su - mohneesh -c "whoami;abuild-keygen -a -i"
+su - mohneesh -c "abuild-keygen -a -i"
 su - mohneesh -c "cd /tmp/jobber/packaging/alpine;abuild checksum;"
 su - mohneesh -c "cd /tmp/jobber/packaging/alpine;abuild -r"
-cd /home/mohneesh/packages/packaging/aarch64/
-pwd
-ls
+ls /home/mohneesh/packages/packaging/aarch64/
